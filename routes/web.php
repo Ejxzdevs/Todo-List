022110a33bfa3824b1cb37e36bb3a36d910def0f
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-10 02:58:37 +0800

    remove unnecessary route

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\TaskController;
 Route::get('/', [TaskController::class,'index'])->name('index');
 Route::prefix('task')->group(function () {
     Route::post('/store', [TaskController::class, 'store'])->name('tasks.store');
-    Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
-    Route::get('/{task}', [TaskController::class, 'viewDetails'])->name('tasks.details');
+    Route::put('/update', [TaskController::class, 'updateDetails'])->name('tasks.update');
     Route::get('/delete/{id}', [TaskController::class, 'deleteDetails'])->name('tasks.delete');
-    Route::patch('/done/{id}', [TaskController::class, 'doneT'])->name('task.done');
-    Route::put('/{task}/edit', [TaskController::class, 'editDetails'])->name('tasks.edit');
-   
 });
